5ad9cb5fa6e4d962b6be33ed0747dc9e8e03fccc
mbairo <bairo.marcos@gmail.com>
2022-10-27 17:55:10 -0300

    criar alerta ao clicar nos emojis

--- app/src/main/java/com/mbairo/sustentabilidadeemocionalemempresas/NivelSentimento.java
+++ app/src/main/java/com/mbairo/sustentabilidadeemocionalemempresas/NivelSentimento.java
@@ -13,7 +13,7 @@ import java.util.Set;
 
 public class NivelSentimento extends AppCompatActivity {
     private ImageView imageView_duvidas, muito_feliz_empty, muito_feliz_full, feliz_empty, feliz_full, meio_chateado_empty, meio_chateado_full, triste_empty, triste_full, muito_triste_empty, muito_triste_full;
-    private TextView textView_pergunta_do_dia, textView_dicas_de_saude;
+    private TextView textView_pergunta_do_dia, textView_dicas_de_saude, textView_alerta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +28,9 @@ public class NivelSentimento extends AppCompatActivity {
             public void onClick(View view) {
                 SetInvisible();
                 muito_feliz_full.setVisibility(View.VISIBLE);
+                // TODO: Buscar dados com o backend API
+                textView_alerta.setText("Texto para o emoji Muito Feliz");
+                textView_alerta.setVisibility(View.VISIBLE);
             }
         });
         feliz_empty.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +38,9 @@ public class NivelSentimento extends AppCompatActivity {
             public void onClick(View view) {
                 SetInvisible();
                 feliz_full.setVisibility(View.VISIBLE);
+                // TODO: Buscar dados com o backend API
+                textView_alerta.setText("Texto para o emoji Feliz");
+                textView_alerta.setVisibility(View.VISIBLE);
             }
         });
         meio_chateado_empty.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +48,9 @@ public class NivelSentimento extends AppCompatActivity {
             public void onClick(View view) {
                 SetInvisible();
                 meio_chateado_full.setVisibility(View.VISIBLE);
+                // TODO: Buscar dados com o backend API
+                textView_alerta.setText("Texto para o emoji Meio Chateado");
+                textView_alerta.setVisibility(View.VISIBLE);
             }
         });
         triste_empty.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +58,9 @@ public class NivelSentimento extends AppCompatActivity {
             public void onClick(View view) {
                 SetInvisible();
                 triste_full.setVisibility(View.VISIBLE);
+                // TODO: Buscar dados com o backend API
+                textView_alerta.setText("Texto para o emoji Triste");
+                textView_alerta.setVisibility(View.VISIBLE);
             }
         });
         muito_triste_empty.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +68,9 @@ public class NivelSentimento extends AppCompatActivity {
             public void onClick(View view) {
                 SetInvisible();
                 muito_triste_full.setVisibility(View.VISIBLE);
+                // TODO: Buscar dados com o backend API
+                textView_alerta.setText("Texto para o emoji Muito Triste");
+                textView_alerta.setVisibility(View.VISIBLE);
             }
         });
 
@@ -94,6 +109,7 @@ public class NivelSentimento extends AppCompatActivity {
         imageView_duvidas = findViewById(R.id.imageView_duvidas);
         textView_pergunta_do_dia = findViewById(R.id.textView_pergunta_do_dia);
         textView_dicas_de_saude = findViewById(R.id.textView_dicas_de_saude);
+        textView_alerta = findViewById(R.id.textView_alerta);
 
         muito_feliz_empty = findViewById(R.id.img_muito_feliz_empty);
         muito_feliz_full = findViewById(R.id.img_muito_feliz_full);
@@ -112,6 +128,7 @@ public class NivelSentimento extends AppCompatActivity {
     }
 
     public void SetInvisible(){
+        textView_alerta.setVisibility(View.INVISIBLE);
         muito_feliz_full.setVisibility(View.INVISIBLE);
         feliz_full.setVisibility(View.INVISIBLE);
         meio_chateado_full.setVisibility(View.INVISIBLE);
